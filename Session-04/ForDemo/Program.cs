c559720ba605fc99d9b9e9f1f99e38db9d8c39be
amirholyanasabnarab <amirholyanasabnarab@gmail.com>
2021-04-16 00:13:11 +0430

    session-04-for

---
+++ Session-04/ForDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter number : ");
+            for(var number =int.Parse(Console.ReadLine());number>1;number--)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Session-04/ForFactoialDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForFactoialDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your number : ");
+            int result = 1;
+            for(var number = int.Parse(Console.ReadLine());number >1;number--)
+            {
+                result *= number;
+            }
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
