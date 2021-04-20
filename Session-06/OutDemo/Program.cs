6e20f9cfc27b3682f961019b387d18f7e47aeeff
amirholyanasabnarab <amirholyanasabnarab@gmail.com>
2021-04-21 01:34:45 +0430

    session-06

---
+++ Session-06/OutDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[100];
+            Random r = new Random();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = r.Next(1000, 2000);
+            }
+            MathOperations(nums, out int max, out int min, out double avg);
+            Console.WriteLine($"max : {max}");
+            Console.WriteLine($"min : {min}");
+            Console.WriteLine($"avg : {avg}");
+
+            Console.ReadKey();
+        }
+        static void MathOperations(int[] numbers , out int max ,out int min , out double avg)
+        {
+            max = numbers[0];
+            min = numbers[0];
+            int sum = 0;
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+                sum += numbers[i];
+            }
+            avg = sum / numbers.Length;
+        }
+    }
+}
---
+++ Session-06/TryParseDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryParseDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter number :");
+            if (int.TryParse(Console.ReadLine(),out int number))
+            {
+                Console.WriteLine(Math.Log(number));
+            }
+            else
+                Console.WriteLine("Please enter a number.");
+            Console.ReadKey();
+        }
+    }
+}
