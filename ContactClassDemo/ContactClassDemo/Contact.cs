6b30d050e44c740584cf9c44ec164a8b42a15c85
amirholyanasabnarab <amirholyanasabnarab@gmail.com>
2021-04-27 00:05:20 +0430

    session-07

---
+++ ContactClassDemo/ContactClassDemo/Contact.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactClassDemo
+{
+    class Contact
+    {
+        private string name;
+        private string family;
+        private string phoneNumber;
+        private string mobile;
+
+        public void setName(string _name)
+        {
+            name = _name;
+        }
+        public string getName()
+        {
+            return name;
+        }
+
+        public void setFamily(string _family)
+        {
+            family = _family;
+        }
+        public string getFamily()
+        {
+            return family;
+        }
+        public void setPhoneNumber(string _phoneNumber)
+        {
+            if (!_phoneNumber.StartsWith("0"))
+            {
+                phoneNumber = "021" + _phoneNumber;
+            }
+            else
+                phoneNumber = _phoneNumber;
+        }
+        public string getPhoneNumber()
+        {
+            return phoneNumber;
+        }
+
+        public void setMobile(string _cellPhone)
+        {
+            if (_cellPhone.Substring(0, 2) == "00")
+                _cellPhone = $"+{_cellPhone.Substring(2, _cellPhone.Length - 2)}";
+            else if (_cellPhone[0] == '0')
+                _cellPhone = $"+98{_cellPhone.Substring(1, _cellPhone.Length - 1)}";
+
+            mobile = _cellPhone;
+        }
+        public string getMobile()
+        {
+            return mobile;
+        }
+    }
+}
---
+++ ContactClassDemo/ContactClassDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contact[] contacts = new Contact[3];
+            for (int i = 0; i < contacts.Length; i++)
+            {
+                Console.Clear();
+                for (int j = 0; j < contacts.Length; j++)
+                {
+                    if (contacts[j]!= null)
+                    {
+                        Console.WriteLine($"{contacts[j].getName()}\t{contacts[j].getFamily()}\t{contacts[j].getPhoneNumber()}\t{contacts[j].getMobile()}");
+                    }
+                    Console.WriteLine("-------------------------------------------------------");
+                    contacts[i] = new Contact();
+                    Console.WriteLine("name : ");
+                    contacts[i].setName(Console.ReadLine());
+                    Console.WriteLine("family : ");
+                    contacts[i].setFamily(Console.ReadLine());
+                    Console.WriteLine("Phone : ");
+                    contacts[i].setPhoneNumber(Console.ReadLine());
+                    Console.WriteLine("mobile : ");
+                    contacts[i].setMobile(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Phone book is full . . .");
+            Console.ReadKey();
+        }
+    }
+}
